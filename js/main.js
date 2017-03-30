15ce0b7af288250bfe0fcde994795d78b5fad6fa
Julia Tretel <jutretel@outlook.com>
2017-03-30 11:32:27 -0300

    add mobile menu

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-console.log('hello');
+// Botão de abrir
+
+var btnOpenMenu = document.querySelector('.btn-open-menu');
+var btnCloseMenu = document.querySelector('.btn-close-menu');
+var menuMobile = document.querySelector('.menu-mobile');
+
+// Abrir menu mobile
+btnOpenMenu.addEventListener('click', function() {
+  menuMobile.classList.add('is-open');
+});
+
+// Fechar menu mobile
+
+btnCloseMenu.addEventListener('click', function() {
+  menuMobile.classList.remove('is-open');
+});
